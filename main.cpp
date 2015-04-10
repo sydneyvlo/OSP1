b748a3bd8e8b23948d46581ef8116811cf2be2ba
Sydney Lo <sydneyvlo@gmail.com>
2015-04-09 22:41:28 -0700

    Got the getcwd system call to work, just need to do some string manipulation to actually print out the prompt.

--- main.cpp
+++ main.cpp
@@ -4,19 +4,25 @@
 
 using namespace std;
 
+#define BUFFER_LEN 80;
+
 int main() {
     // ssize_t write(int fd, const void *buf, size_t count);
     const char *buf = "osh>";
+    // A c string of the path from the home directory to the working directory.
+    char wd[80];
+    getcwd(wd, 80);
+
+    string wds = wd;
+
+    cout << "size of: " << wds.length() << endl;
     // Check to see if the return value of write which tells us if the function succeeded or not.
     size_t nr;
     // should_run intially set to 1, so our program will at least iterate once.
     int should_run = 1;
 
-    string userInput="";
-
-
     do {
-        write(1, buf, 4);
-        read(0, &userInput, 80);
+//        write(1, wd, 80);
+//        read(0, wd, 80);
     } while(should_run);
 }
