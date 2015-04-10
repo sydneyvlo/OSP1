5301585f54a70d46a149b50a5bca7bb35128e2cf
Sydney Lo <sydneyvlo@gmail.com>
2015-04-09 21:29:11 -0700

    Somewhat got write and read system calls to work except that I'm messing up using the pointers.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <unistd.h>
+#include <string>
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+    // ssize_t write(int fd, const void *buf, size_t count);
+    const char *buf = "osh>";
+    // Check to see if the return value of write which tells us if the function succeeded or not.
+    size_t nr;
+    // should_run intially set to 1, so our program will at least iterate once.
+    int should_run = 1;
+
+    string userInput="";
+
+
+    do {
+        write(1, buf, 4);
+        read(0, &userInput, 80);
+    } while(should_run);
 }
