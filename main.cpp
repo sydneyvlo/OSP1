7735e4367f928f8dc6d3d8d3e919216198fd6389
Sydney Lo <sydneyvlo@gmail.com>
2015-04-10 19:58:46 -0700

    Created a prompt function and main now calls prompt to output the prompt message.

--- main.cpp
+++ main.cpp
@@ -7,31 +7,12 @@ using namespace std;
 
 int BUFFER_SIZE = 256;
 
+void prompt();
+
 int main() {
-    // C string to store the current working directory
-    char wd[BUFFER_SIZE];
 
-    // Get the current working directory
-    getcwd(wd, BUFFER_SIZE);
-
-    // Store wd into a c++ string and get the length of the working directory.
-    string wd2 = wd;
-    string prompt = wd2;
-    int wd2Len = wd2.length();
-
-    if (wd2Len > 16) {
-        // Find the position of the last occurrence of a / symbol.
-        int pos = wd2.rfind('/');
-        prompt = "/..." + wd2.substr(pos) + "/>";
-//        printf("%s", prompt.c_str());
-    }
-
-    // Writing out the prompt to the screen.
-    write(STDOUT_FILENO, prompt.c_str(), prompt.length());
-
-    // temporary cin for testing to see how the prompt looks like.
-//    string input;
-//    cin >> input;
+    // Print out the prompt.
+    prompt();
 
     // C string to store the users input.
     char ui[BUFFER_SIZE];
@@ -42,4 +23,26 @@ int main() {
 
 
 //    printf("%s is of length: %d \n", wd2.c_str(), int(wd2.length()));
+}
+
+void prompt() {
+    // C string to store the current working directory
+    char wd[BUFFER_SIZE];
+
+    // Get the current working directory
+    getcwd(wd, BUFFER_SIZE);
+
+    // Store wd into a c++ string and get the length of the working directory.
+    string prompt = wd;
+    int promptLen = prompt.length();
+
+    if (promptLen > 16) {
+        // Find the position of the last occurrence of a / symbol.
+        int pos = prompt.rfind('/');
+        prompt = "/..." + prompt.substr(pos) + "/>";
+//        printf("%s", prompt.c_str());
+    }
+
+    // Writing out the prompt to the screen.
+    write(STDOUT_FILENO, prompt.c_str(), prompt.length());
 }
